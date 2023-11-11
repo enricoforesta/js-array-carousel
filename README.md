7fd183f26c20a337cb1c74c4d81986dcf00f4fbb
enricoforesta <enrico.foresta97@gmail.com>
2023-11-11 15:53:55 +0100

    feat: Milestone 2 + bonus 1

--- README.md
+++ README.md
@@ -15,11 +15,24 @@ Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, c
 
 ## Logica
 
+### MILESTONE 1
+
+
 1. Avendo già Html e Css, leggiamo il codice e capiamo come è stato scritto, le classi associate e in caso ottimizzare qualcosa.
 
 2. Creiamo il nostro Array d'immagini.
 
 2. Eliminiamo il markup dall html e lo aggiugniamo con JS. 
  - Dichiariamo una costante, collegata all html. 
- - Creo un ciclo per Creare i "<div>"
- - Aggiungo la classe ".active" che servira per mostrare le immagini.
+ - Creo un ciclo per Creare i "<div>".
+ - Aggiungo la classe ".active" che servira per mostrare le immagini che in base a una variabile aggiunge la classe solo all immagine da mostrare. 
+
+### MILESTONE 2
+
+1. Dichiaro due variabili, e le collego alle freccie che useremo per scorrere le immagini.
+
+2. Scateniamo due eventi.
+ - Il primo colleghiamo la prima variabile, e ogni volta che viene cliccato, la variabile "elementCorrente" aumenta di 1.
+ - Il secondo colleghiamo la prima variabile, e ogni volta che viene cliccato, la variabile "elementCorrente" diminiusce di 1.
+ 
+
--- js/main.js
+++ js/main.js
@@ -9,21 +9,47 @@
 const immagini = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg",];
 console.log({ immagini });
 
-// Variabili Container Immagini
+// Variabile Container Immagini
 const elementContainer = document.querySelector(".items");
 console.log({ elementContainer });
 
-
 // Ciclo per creare i div dentro gli items
+for (let i = 0; i < immagini.length; i++) {
 
-for (let i = 0; i < immagini.length - 1; i++) {
-
-    elementContainer.innerHTML += (`<div class="item">
+  elementContainer.innerHTML += (`<div class="item">
     <img src="./img/0${i + 1}.jpg" alt="" />
   </div>`);
 }
 
+//Variabile immagine corrente
+let elementCorrente = 0;
+
 // Aggiungo la classe Active
-const elementActive = document.querySelector(".item");
-elementActive.classList.add("active")
-console.log(elementActive)
+const elementActive = document.querySelectorAll(".item");
+
+elementActive[elementCorrente].classList.add("active");
+console.log(elementCorrente)
+
+// Creo le variabili collegate alle frecce
+const elementUp = document.querySelector(".next");
+const elementDown = document.querySelector(".prev");
+console.log({ elementUp });
+console.log({ elementDown });
+
+// CON  IL MODULO "%" ELEMENTCORRENTE PUò AUMENTARE FINO ALLA LUNGHEZZA DELL ARRAY, SUPERATO LA LUGNHEZZA RITORNA ALL INIZIO. (??????????)
+elementUp.addEventListener("click", function () {
+  elementActive[elementCorrente].classList.remove("active");
+  elementCorrente = (elementCorrente + 1) % elementActive.length;
+  elementActive[elementCorrente].classList.add("active");
+  console.log(elementCorrente)
+}
+)
+
+// CON  IL MODULO "%" ELEMENTCORRENTE PUò DIMUNIRE FINO ALLA LUNGHEZZA DELL ARRAY, MA SE IL RISULTATO E' NEGATIVO DA ERRORE, QUINDI AGGIUNGIAMO LA LUNGHEZZA STESSA,COSI RESTITUISCE IL VALORE DI INIZIALE. (??????????)
+elementDown.addEventListener("click", function () {
+  elementActive[elementCorrente].classList.remove("active");
+  elementCorrente = (elementCorrente - 1 + elementActive.length) % elementActive.length;
+  elementActive[elementCorrente].classList.add("active");
+  console.log(elementCorrente)
+}
+)
